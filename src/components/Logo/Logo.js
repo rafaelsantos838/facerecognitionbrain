c724fe3a390f216a81bd72e8d91bcc6b54a12542
Rafael Santos <rafaelmartins838.rs@gmail.com>
2021-08-10 18:51:43 -0300

    facerecognition creating Components

---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Tilt from 'react-parallax-tilt';
+
+const Logo = () => {
+    return (
+        <div className="ma4 mt0">
+            <Tilt>
+            <div style={{ height: '150px', width:'150px', backgroundColor: 'darkgreen' }}>
+                <h1>React Parallax Tilt 👀</h1>
+            </div>
+            </Tilt>
+        </div>
+    );
+}
+
+export default Logo;
