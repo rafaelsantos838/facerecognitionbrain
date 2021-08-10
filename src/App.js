a9bb69f91c3957434ec60c6c096f5de76519e317
Rafael Santos <rafaelmartins838.rs@gmail.com>
2021-08-10 20:42:28 -0300

    facerecognition creating Components 2

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      {/*<ImageLinkForm />
-      <Facerecognition />*/}
-    </div>
-  );
+const particlesOptions = { 
+  "particles": {
+    "number": {
+        "value": 200
+    },
+    "size": {
+        "value": 2
+    }
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Particles className='particles'
+              params={particlesOptions}
+        />
+
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm />
+        {/*<Facerecognition />*/}
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import './Logo.css'
+import wale from './wale.png';
 
 const Logo = () => {
     return (
         <div className="ma4 mt0">
-            <Tilt>
-            <div style={{ height: '150px', width:'150px', backgroundColor: 'darkgreen' }}>
-                <h1>React Parallax Tilt 👀</h1>
+            <Tilt className="Tilt br2 shadow-2" options={{max:55}} style={{ height: '150px', width:'150px' }}>
+            <div className="Tilt-inner pa3">
+                <img src={wale} alt="logo" />
             </div>
             </Tilt>
         </div>
